2b7f149fd287c5338724b09daaa68c7c3a3ab21f
Bob Steers <steers.bob@gmail.com>
2020-08-28 11:47:41 +0200

    better install prompt if loader failed

--- brewblox_ctl_lib/loader.py
+++ brewblox_ctl_lib/loader.py
@@ -5,10 +5,10 @@ Collects and returns all CLI groups in the correct order
 try:
     from brewblox_ctl_lib import const, utils  # noqa: F401
 except AttributeError as ex:  # pragma: no cover
-    CTL_VERSION = '0.21.0'  # can't be placed in const
+    CTL_VERSION = '0.22.2'  # can't be placed in const
     print('Failed to import dependency from brewblox-ctl.')
     print('The minimum required version is {}.'.format(CTL_VERSION))
-    print('To update brewblox-ctl, run: \n\n\t pip3 install --user --upgrade --no-cache-dir brewblox-ctl \n')
+    print('To update brewblox-ctl, run: \n\n\t python3 -m pip install --user --upgrade --no-cache-dir brewblox-ctl \n')
     raise ex
 
 from brewblox_ctl_lib.commands import (add_device, backup, diagnostic, service,
